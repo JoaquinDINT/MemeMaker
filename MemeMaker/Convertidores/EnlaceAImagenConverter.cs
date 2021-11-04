7fdf109e1f108031713a7b3d88674b12cb35159c
JAcutillas <ja.cutillas@iesdoctorbalmis.com>
2021-11-04 08:30:33 +0100

    Primeros cambios

---
+++ MemeMaker/Convertidores/EnlaceAImagenConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MemeMaker.Convertidores
+{
+    public class EnlaceAImagenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string enlaceString = value.ToString();
+            BitmapImage source = new BitmapImage(new Uri("assets/2560.jpg", UriKind.Relative));
+
+            if (Uri.TryCreate(enlaceString, UriKind.Absolute, out Uri enlaceUri))
+            {
+                source = new BitmapImage(enlaceUri);
+            }
+
+            return source;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
